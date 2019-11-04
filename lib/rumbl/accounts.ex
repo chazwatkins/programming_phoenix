be220c161d654b5cdc7143709a7582f10a1da9d0
Chaz Watkins <chazwatkins@live.com>
2019-11-03 22:16:06 -0600

    chapter 10 - display users and finished

--- lib/rumbl/accounts.ex
+++ lib/rumbl/accounts.ex
@@ -2,6 +2,8 @@ defmodule Rumbl.Accounts do
   @moduledoc """
   The Accounts context.
   """
+  import Ecto.Query
+
   alias Rumbl.Repo
   alias Rumbl.Accounts.User
 
@@ -35,6 +37,10 @@ defmodule Rumbl.Accounts do
 
   def list_users do
     Repo.all(User)
+  end
+
+  def list_users_with_ids(ids) do
+    Repo.all(from u in User, where: u.id in ^ids)
   end
 
   def authenticate_by_username_and_pass(username, given_pass) do
